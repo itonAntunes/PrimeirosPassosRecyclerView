18236ad1f47bded3c23f3dd0170b546157a92787
Everton Antunes <iton.antunes@gmail.com>
2021-10-13 20:43:24 -0300

    adapter

--- app/src/main/java/com/example/recicleviewaplication/AdapterAlunos.kt
+++ app/src/main/java/com/example/recicleviewaplication/AdapterAlunos.kt
@@ -1,9 +1,22 @@
 package com.example.recicleviewaplication
 
 import android.content.Context
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class AdapterAlunos(mainActivity: Context, alunos: List<String>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class AdapterAlunos(mainActivity: Context, alunos: List<String>)
+    : RecyclerView.Adapter<AdapterAlunos> {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AdapterAlunos {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: AdapterAlunos, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
 
 
 }
--- app/src/main/java/com/example/recicleviewaplication/AlunoViewHolder.kt
+++ app/src/main/java/com/example/recicleviewaplication/AlunoViewHolder.kt
@@ -1,10 +1,11 @@
 package com.example.recicleviewaplication
 
 import android.view.View
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class AlunoViewHolder(view: View):RecyclerView.ViewHolder(view) {
 
-    var nomeAluno = view.findViewById<TextView>()
+    var nomeAluno = view.findViewById<TextView>(R.id.item_text)
 
 }
